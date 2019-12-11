82600c4f89894b1ce59a2f2426610f0a340511a8
Jorge Walter Guzman <jwguzman@gmail.com>
2019-12-11 20:37:50 -0300

    Base proyect

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
